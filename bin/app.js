b9bd6adc77381e71057e63f20ff19e0e894028d8
dkk934 <dkking934@gmail.com>
2024-04-11 23:32:49 +0530

    fitst commit

---
+++ bin/app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import { exec } from 'child_process';
+
+let commands = ['git init', 'git status', 'git add .','git status'];
+
+let execPromisified = (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                reject(error);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                reject(stderr);
+            }
+            console.log(`stdout: ${command} = ${stdout}`);
+            resolve(stdout);
+        });
+    });
+};
+
+(async () => {
+    for (let command of commands) {
+        try {
+            await execPromisified(command);
+        } catch (error) {
+            console.log(`An error occurred: ${error}`);
+        }
+    }
+    console.log('All commands executed.');
+})();
